667541b10c45a181d9ea2e09c2064f05ac63829b
Mike Lewis <mlewis@sandow.com>
2015-03-19 14:35:52 +0000

    Fix django conf urls

---
+++ README.rst
@@ -0,0 +1,29 @@
+django-selector is a custom url pattern parser for Django that is based off of
+`Luke Arno's Selector <http://github.com/lukearno/selector/>`_ for WSGI.  It
+is designed to simplify the writing and reading of url patterns by providing
+recipes for frequently used patterns.  django-selector's parser ignores classic
+regex based url patterns, so if you require the flexibility of regexes you
+needn't jump through registration hoops for a one-off url pattern. Using these
+named patterns in your urls.py clarifies *what* they are matching as well as
+*how* they are matching it::
+
+    patterns('foo.views',
+    (r'^/(?P<name>[a-zA-Z0-9\-]+)/(?P<foos>\d*.?\d+)/$', 'index', {}, 'foo-index'))
+
+becomes::
+
+    parser.patterns('foo.views',
+    (r'/{name:slug}/{foos:number}/', 'index', {}, 'foo-index'))
+
+You can install django-selector with pip::
+
+    pip install django-selector
+
+You can fork django-selector `from its hg repository
+<http://bitbucket.org/jmoiron/django-selector>`_::
+
+    hg clone http://bitbucket.org/jmoiron/django-selector
+
+You can also read the full `release documentation 
+<http://packages.python.org/django-selector/>`_.
+
---
+++ setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from setuptools import setup
+#from distutils.core import setup
+
+version = '0.4'
+
+path = os.path.dirname(__file__)
+if not path: path = '.'
+readme = open(path + '/README.rst', 'r').read()
+
+setup(name='django-selector',
+      version=version,
+      description="django urls helper based on wsgi selector",
+      long_description=readme,
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python',
+          'Operating System :: OS Independent',
+          'Framework :: Django',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+      ],
+      keywords='django urls selector',
+      author='Jason Moiron',
+      author_email='jmoiron@jmoiron.net',
+      url='http://bitbucket.org/jmoiron/django-selector/',
+      license='MIT',
+      py_modules=['dselector'],
+      # setuptools specific
+      zip_safe=False,
+      test_suite = "tests",
+      # django is a requirement for this but.. surely...
+      install_requires=['django'] # , 'lxml'],
+)
+
+
